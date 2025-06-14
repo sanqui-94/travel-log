23ee7803a2c8b7ebe6747a9ecaf4be36282fb403
Santiago Quiroga Turdera <sanqui.94@gmail.com>
2025-06-14 00:02:28 -0400

    removing un-used import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Icon } from '@iconify/react';
 import tablerAntenna from "@iconify-icons/tabler/antenna";
 import tablerAntennaOff from "@iconify-icons/tabler/antenna-off";
-import { MapIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
     return (
@@ -10,14 +9,6 @@ export default function Home() {
 
             <button className="btn btn-primary">Primary<Icon icon={tablerAntenna} className="w-5 h-5 text-gray-200" /></button>
             <button className="btn btn-primary">Primary<Icon icon={tablerAntennaOff} className="w-5 h-5 text-gray-200" /></button>
-            <button className="btn btn-secondary">Secondary <MapIcon className="size-6 text-blue-500"/></button>
-            <button className="btn btn-secondary">Secondary <svg xmlns="http://www.w3.org/2000/svg" width="1em"
-                                                                 height="1em"
-                                                                 viewBox="0 0 24 24">
-                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="m3 7l6-3l6 3l6-3v13l-6 3l-6-3l-6 3zm6-3v13m6-10v13"/>
-            </svg>
-            </button>
         </h1>
     )
 }
