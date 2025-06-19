3ca25eeef7c864ffd0fc3d188c3b36e3f6022b29
Santiago Quiroga Turdera <sanqui.94@gmail.com>
2025-06-19 12:12:29 -0400

    Theme swapper added

--- client/src/components/buttons/LoginButton.tsx
+++ client/src/components/buttons/LoginButton.tsx
@@ -2,7 +2,7 @@ import GithubIcon from "~icons/tabler/brand-github";
 
 export default function LoginButton() {
   return (
-    <button className="btn btn-secondary">
+    <button className="btn btn-accent">
       Sign In with Github
       <GithubIcon height={24} width={24} />
     </button>
