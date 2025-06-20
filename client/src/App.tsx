e6897eb2bf3c4343308d0373c5b48456592a79fb
Santiago Quiroga Turdera <sanqui.94@gmail.com>
2025-06-20 10:38:26 -0400

    removing unused console.log

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
 import "./App.css";
 import AppRouter from "./router";
-import { env } from "../lib/env.ts";
 
 function App() {
-  console.log(env);
   return <AppRouter />;
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,7 @@
-import { env } from "../lib/env.ts";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
-console.log(env);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
