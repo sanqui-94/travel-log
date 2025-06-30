4568d4ddfada9eb2bec2af5e45b6f08def932795
Santiago Quiroga Turdera <sanqui.94@gmail.com>
2025-06-30 16:14:19 -0400

    new button if user already signed in

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
-import LoginButton from "@/app/components/buttons/LoginButton";
 import NavBar from "@/app/components/navigation/NavBar";
+import HeroSection from "@/app/components/HeroSection";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <div className="hero bg-base-300 container mx-auto mt-4">
-        <div className="hero-content min-h-96 text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Travel Log</h1>
-            <p className="py-6">
-              Keep track of your travels and adventures. Add locations, photos, and notes to create
-              a digital journal of your journeys.
-            </p>
-            <LoginButton />
-          </div>
-        </div>
-      </div>
+      <HeroSection />
     </>
   );
 }
